76c3b7deadb86e6a4f3e0ac894a393179474e618
rsnorman <rsnorman15@gmail.com>
2015-01-29 00:14:40 -0700

    Added a couple more api tests

--- spec/api/birthdays_spec.rb
+++ spec/api/birthdays_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Birthdays API" do
+  def get_birthdays(client)
+    get '/v1/birthdays', {}, {"HTTP_UUID" => client.uuid}
+  end
 
   describe "GET /birthdays" do
     let(:client) { create :client }
     it 'sends a list of birthdays for the week' do
       2.times { create(:person, client: client) }
 
-      get '/v1/birthdays', {}, {"HTTP_UUID" => client.uuid}
+      get_birthdays(client)
 
       expect(response).to be_success
       expect(response_json.length).to eq(2)
@@ -18,8 +21,44 @@ RSpec.describe "Birthdays API" do
         person = create(:person, client:        client,
                                  name:          "Jerry Seinfeld",
                                  date_of_birth: Date.parse("1954-4-29") )
-        get '/v1/birthdays', {}, {"HTTP_UUID" => client.uuid}
+        get_birthdays(client)
+
         expect(response_json).to eq [birthday_json(person)]
+      end
+    end
+
+    context "with birthdays before this week" do
+      before { create(:person, client:        client,
+                               date_of_birth: Date.current.beginning_of_week - 30.years - 1.day) }
+
+      it "should return no birthdays" do
+        get_birthdays(client)
+
+        expect(response).to be_success
+        expect(response_json.length).to eq 0
+      end
+    end
+
+    context "with birthdays after this week" do
+      before { create(:person, client:        client,
+                               date_of_birth: Date.current.end_of_week - 30.years + 1.day) }
+
+      it "should return no birthdays" do
+        get_birthdays(client)
+
+        expect(response).to be_success
+        expect(response_json.length).to eq 0
+      end
+    end
+
+    context "with birthdays from a different client" do
+      before { create(:person, client: create(:client)) }
+
+      it "should return no birthdays" do
+        get_birthdays(client)
+
+        expect(response).to be_success
+        expect(response_json.length).to eq 0
       end
     end
   end
