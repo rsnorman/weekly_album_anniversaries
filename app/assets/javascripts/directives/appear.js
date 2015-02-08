de070b70916d216553778308d529d4595562f5b1
rsnorman <rsnorman15@gmail.com>
2015-02-07 19:11:10 -0700

    Added appear directive for mobile

---
+++ app/assets/javascripts/directives/appear.js
@@ -0,0 +1,28 @@
+(function(ng, module, $) {
+  'use strict';
+
+  module.directive('appear', ['$window', function($window) {
+    return {
+      restrict: 'A',
+      link: function($scope, $element) {
+        function checkVisible() {
+          var docViewTop = $($window).scrollTop();
+          var docViewBottom = docViewTop + $($window).height();
+          var padding = 200;
+
+          var elemTop = $element.offset().top;
+          var elemBottom = elemTop + $element.height();
+
+          if ((elemBottom <= docViewBottom - padding) && (elemTop >= docViewTop + padding)) {
+            $element.addClass('appear');
+          } else {
+            $element.removeClass('appear');
+          }
+        }
+
+        $($window).scroll(checkVisible).resize(checkVisible);
+      }
+    }
+  }]);
+
+})(angular, angular.module('Norm.WeeklyAnniversary'), jQuery);
