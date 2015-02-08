4d596b2a5d0df31b4e6032a86849e9b331921e76
rsnorman <rsnorman15@gmail.com>
2015-02-07 18:19:06 -0700

    Styled better for small screens

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207020109) do
+ActiveRecord::Schema.define(version: 20150207020113) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name",                                 null: false
