4675d326115d3a94af97328b96a3da3be4c015bf
Ryan Norman <rsnorman15@gmail.com>
2016-02-28 14:50:59 -0500

    DRY up angular load albums code

--- app/assets/javascripts/controllers/weekly-anniversaries.js
+++ app/assets/javascripts/controllers/weekly-anniversaries.js
@@ -2,26 +2,34 @@
   'use strict';
 
   function WeeklyAnniversariesCtrl ($scope, Anniversary, $timeout, ImageLoader) {
+    function loadAlbumImages(albums) {
+      return ImageLoader.load(albums.map(function(album) {
+        return album.thumbnail_url;
+      }));
+    }
+
+    function showAlbumAnniversaries(anniversaries) {
+      $scope.isLoading = false;
+      $scope.isLoadingNext = false;
+      $scope.isLoadingPrevious = false;
+
+
+      $scope.weekStart = anniversaries.week_start;
+      $scope.weekEnd = anniversaries.week_end;
+      $scope.weekNumber = anniversaries.week_number;
+
+      $scope.nextDisabled = $scope.weekNumber === 52;
+      $scope.prevDisabled = $scope.weekNumber === 1;
+
+      $scope.albumAnniversaries = anniversaries.albums;
+    }
+
     function getAnniversaries() {
       $scope.isLoading = true;
 
       Anniversary.all($scope.weekNumber).success(function(anniversaries) {
-        ImageLoader.load(anniversaries.albums.map(function(album) {
-          return album.thumbnail_url;
-        })).then(function() {
-          $scope.isLoading = false;
-          $scope.isLoadingNext = false;
-          $scope.isLoadingPrevious = false;
-
-
-          $scope.weekStart = anniversaries.week_start;
-          $scope.weekEnd = anniversaries.week_end;
-          $scope.weekNumber = anniversaries.week_number;
-
-          $scope.nextDisabled = $scope.weekNumber === 52;
-          $scope.prevDisabled = $scope.weekNumber === 1;
-
-          $scope.albumAnniversaries = anniversaries.albums;
+        loadAlbumImages(anniversaries.albums).then(function() {
+          showAlbumAnniversaries(anniversaries);
         });
       });
     }
@@ -52,24 +60,11 @@
     $scope.$watch('query', function(query) {
       if ( query && query.length > 3 ) {
         $scope.isLoading = true;
+        $scope.albumAnniversaries = [];
 
         Anniversary.search(query).success(function(anniversaries) {
-          ImageLoader.load(anniversaries.albums.map(function(album) {
-            return album.thumbnail_url;
-          })).then(function() {
-            $scope.isLoading = false;
-            $scope.isLoadingNext = false;
-            $scope.isLoadingPrevious = false;
-
-
-            $scope.weekStart = anniversaries.week_start;
-            $scope.weekEnd = anniversaries.week_end;
-            $scope.weekNumber = anniversaries.week_number;
-
-            $scope.nextDisabled = $scope.weekNumber === 52;
-            $scope.prevDisabled = $scope.weekNumber === 1;
-
-            $scope.albumAnniversaries = anniversaries.albums;
+          loadAlbumImages(anniversaries.albums).then(function() {
+            showAlbumAnniversaries(anniversaries);
           });
         });
       }
