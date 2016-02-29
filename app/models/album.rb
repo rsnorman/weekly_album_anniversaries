3e7cbc42621b1c23e930ee1bc74155ca1bd442d3
Ryan Norman <rsnorman15@gmail.com>
2016-02-28 22:47:54 -0500

    Add slug to album

--- app/models/album.rb
+++ app/models/album.rb
@@ -7,13 +7,20 @@ class Album < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :artist
   validates_presence_of :release_date
+  validates_presence_of :slug
   validates :release_date, release_date: true
-  validates_uniqueness_of :name, :scope => :artist
+  validates_uniqueness_of :name, scope: :artist
+
+  before_validation :set_slug
 
   # Returns anniversary of album
   # @returns [Anniverary] anniversary of album
   def anniversary
     @anniversary ||= Anniversary.new(release_date)
+  end
+
+  def set_slug
+    self.slug = "#{artist.underscore}-#{name.underscore}"
   end
 
 end
