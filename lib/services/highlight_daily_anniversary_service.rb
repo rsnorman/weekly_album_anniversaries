552482d17c9ba68109f9942e49ed606c5028430d
Ryan Norman <rsnorman15@gmail.com>
2016-03-02 23:19:46 -0500

    Share album image with tweet
    Why
    ---
    Tweets get noticed more if there is an image attached to them
    
    This change addresses the need by
    ---------------------------------
    Attaches an image for daily album anniversary

--- lib/services/highlight_daily_anniversary_service.rb
+++ lib/services/highlight_daily_anniversary_service.rb
@@ -4,15 +4,50 @@ class HighlightDailyAnniversaryService
   def tweet
     return if highlighted_album.nil?
     HighlightedAlbum.create(album: highlighted_album)
-    client.update(
-      "#{highlighted_album.artist}'s \"#{highlighted_album.name}\" " \
-      "turns #{pluralize(highlighted_album.anniversary.count, 'years')} old this week. " \
-      "https://wistfulindie.herokuapp.com/albums/#{highlighted_album.slug} " \
-      "#indiemusic #classicindie"
-    )
+    client.update_with_media(tweet_content, album_image_file)
+  ensure
+    delete_album_image
   end
 
   private
+
+  def tweet_content
+    "#{artist_possesive} \"#{highlighted_album.name}\" " \
+    "turns #{pluralize(highlighted_album.anniversary.count, 'years')} old this week. " \
+    "https://wistfulindie.herokuapp.com/albums/#{highlighted_album.slug} " \
+    "#indiemusic #classicindie"
+  end
+
+  def artist_possesive
+    name = highlighted_album.artist
+    name[-1] == 's' ? "#{name}'" : "#{name}'s"
+  end
+
+  def album_image_file
+    return @album_image_file if @album_image_file
+    save_album_image
+    @album_image_path = File.open(album_image_path)
+  end
+
+  def save_album_image
+    File.open(album_image_path, 'w', encoding: 'ascii-8bit') do |f|
+      f << download_album_image
+    end
+  end
+
+  def delete_album_image
+    album_image_file.close
+    File.unlink(album_image_file)
+  end
+
+  def download_album_image
+    require 'open-uri'
+    open(highlighted_album.thumbnail) { |f| f.read }
+  end
+
+  def album_image_path
+    "./tmp/#{highlighted_album.slug}.jpg"
+  end
 
   def highlighted_album
     @highlighted_album ||=
