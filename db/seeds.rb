5657ef1fb010d421eba4d50f4700d12ae09cb93c
Ryan Norman <rsnorman15@gmail.com>
2016-03-05 22:54:36 -0500

    Make it very easy to set twitter screen name for artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,18 @@
+content = File.read('./db/seed_data/artists.json').strip
+artists = JSON.parse(content)
 
+artists.each do |a|
+  ar = Artist.find_by(name: a['name'])
+  ar && ar.update(twitter_screen_name: a['twitter_screen_name'])
+end
 
+artists.each do |a|
+  ar = Artist.find_by(name: a['name'])
+  ar && a['potential_twitter_screen_names'].each do |sn|
+    PotentialTwitterScreenName.create(
+      artist: ar,
+      screen_name: sn['screen_name'],
+      strength: sn['strength']
+    )
+  end
+end
