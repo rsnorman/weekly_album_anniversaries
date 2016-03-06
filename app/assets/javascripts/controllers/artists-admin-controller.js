3164edc324fdbccbb59dfa7c418310d6efc2d2b4
Ryan Norman <rsnorman15@gmail.com>
2016-03-05 23:13:28 -0500

    Allow updating screen name

--- app/assets/javascripts/controllers/artists-admin-controller.js
+++ app/assets/javascripts/controllers/artists-admin-controller.js
@@ -7,11 +7,27 @@
     });
 
     $scope.setTwitterScreenName = function(artist, screenName) {
-      Artist.update(artist, {
+      return Artist.update(artist, {
         twitter_screen_name: screenName
       }).success(function() {
         artist.twitter_screen_name = screenName;
       });
+    };
+
+    $scope.editTwitterScreenName = function(artist) {
+      artist.isEditing = true;
+      artist.previousTwitterScreenName = artist.twitter_screen_name;
+    };
+
+    $scope.updateTwitterScreenName = function(artist) {
+      $scope.setTwitterScreenName(artist, artist.twitter_screen_name).success(function() {
+        artist.isEditing = false;
+      });
+    };
+
+    $scope.cancelTwitterScreenNameEdit = function(artist) {
+      artist.isEditing = false;
+      artist.twitter_screen_name = artist.previousTwitterScreenName;
     };
   }
 
