2e98abe130b481fe642cc52a8bd587413c43c6f8
Ryan Norman <rsnorman15@gmail.com>
2016-03-06 13:14:36 -0500

    Add artist hash tag

--- lib/services/highlight_daily_anniversary_service.rb
+++ lib/services/highlight_daily_anniversary_service.rb
@@ -1,26 +1,51 @@
+require './lib/wistful_indie/twitter/client'
+
 class HighlightDailyAnniversaryService
   include ActionView::Helpers::TextHelper
 
   def tweet
     return if highlighted_album.nil?
     HighlightedAlbum.create(album: highlighted_album)
-    client.update_with_media(tweet_content, album_image_file)
+    puts "Tweeting for #{highlighted_album.artist.name} - #{highlighted_album.name}"
+    begin
+      client.update_with_media(tweet_content, album_image_file)
+    rescue Twitter::Error::Forbidden => e
+      if e.message == 'Status is over 140 characters.'
+        client.update_with_media(tweet_content(short: true), album_image_file)
+      end
+    end
   ensure
-    delete_album_image
+    delete_album_image if highlighted_album
   end
 
   private
 
-  def tweet_content
+  def tweet_content(short: false)
     "#{artist_possesive} \"#{highlighted_album.name}\" " \
     "turns #{pluralize(highlighted_album.anniversary.count, 'years')} old this week. " \
     "https://wistfulindie.herokuapp.com/albums/#{highlighted_album.slug} " \
-    "#indiemusic #classicindie"
+    "#{hash_tags(without_artist: short)}"
   end
 
   def artist_possesive
-    name = highlighted_album.artist
+    name = artist_name
     name[-1] == 's' ? "#{name}'" : "#{name}'s"
+  end
+
+  def artist_name
+    if (screen_name = highlighted_album.artist.twitter_screen_name)
+      ".@#{screen_name}"
+    else
+      highlighted_album.artist.name
+    end
+  end
+
+  def hash_tags(without_artist: false)
+    unless without_artist
+      "#indiemusic ##{highlighted_album.artist.name.gsub(/[^a-zA-Z]*/, '')}"
+    else
+      '#indiemusic'
+    end
   end
 
   def album_image_file
@@ -50,10 +75,7 @@ class HighlightDailyAnniversaryService
   end
 
   def highlighted_album
-    @highlighted_album ||=
-      WeeklyAnniversaryQuery.new(unhighlighted_albums).find_all.detect do |album|
-        album.anniversary.current == Date.current
-      end
+    @highlighted_album ||= Album.all.to_a.sample
   end
 
   def unhighlighted_albums
