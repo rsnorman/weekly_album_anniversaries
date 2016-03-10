10982a64d394dc04ffa3f893f01cd3f26b2acd0d
Ryan Norman <rsnorman15@gmail.com>
2016-03-09 23:26:52 -0500

    Fix broken album search to work with artists table
    Why
    ---
    Album search stopped working once artists was broken into a separate
    table
    
    This change addresses the need by
    ---------------------------------
    TDD fixing the `AlbumSearch` class to handle `artists` table

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,15 @@
 FactoryGirl.define do
   factory :album do
-    sequence(:name) {|n| "Kid A #{n}" }
-    artist "Radiohead"
+    sequence(:name) { |n| "Kid A #{n}" }
+    artist
     release_date Date.current - 30.years
     thumbnail "thumbnail.jpg"
     rating 8.8
     link "http://pitchfork.com/review/kida"
+  end
+
+  factory :artist do
+    sequence(:name) { |n| "Radiohead #{n}" }
   end
 
   factory :genre do
