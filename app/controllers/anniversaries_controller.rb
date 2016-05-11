4562bdc1c2513fa66c5a598be3ae31f0e020fa8c
Ryan Norman <rsnorman15@gmail.com>
2016-05-10 20:33:22 -0400

    Enable caching for anniversaries and highllighted album

--- app/controllers/anniversaries_controller.rb
+++ app/controllers/anniversaries_controller.rb
@@ -3,24 +3,34 @@ class AnniversariesController < ApplicationController
 
   # Returns a list of all the anniversary for the current week
   def index
-    render json: api_json_for(anniversaries.find_all)
+    render json: api_json_for_anniversaries
   end
 
   private
 
   def anniversaries
-    @query ||= WeeklyAnniversaryQuery.new(Album.all, week)
+    @anniversaries ||=
+      WeeklyAnniversaryQuery.new(Album.all.includes(:artist), week).find_all
   end
 
   def week
-    params[:week_number].nil? ?
-    Week.current :
-    Week.from_week_number(params[:week_number].to_i)
+    if params[:week_number].nil?
+      Week.current
+    else
+      Week.from_week_number(params[:week_number].to_i)
+    end
   end
 
-  def api_json_for(albums)
-    decorater = AlbumAnniversariesJsonDecorator.new(albums, week)
-    decorater.to_api_json
+  def api_json_for_anniversaries
+    cache(weekly_album_json_cache_key) do
+      AlbumAnniversariesJsonDecorator.new(anniversaries, week).to_api_json
+    end
+  end
+
+  def weekly_album_json_cache_key
+    count          = anniversaries.count
+    max_updated_at = anniversaries.sort_by(&:updated_at).last
+    "anniversaries-json/all-#{count}-#{max_updated_at}-#{week.number}"
   end
 
 end
