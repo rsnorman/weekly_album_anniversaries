a69be235122b638adad1b0b9c665702a95bbd27d
Ryan Norman <rsnorman15@gmail.com>
2016-05-10 20:33:22 -0400

    Enable caching for anniversaries and highllighted album

--- app/queries/weekly_anniversary_query.rb
+++ app/queries/weekly_anniversary_query.rb
@@ -11,13 +11,23 @@ class WeeklyAnniversaryQuery
   # Finds all the records with anniversary during the current week
   # @return [Array<Album>] array of albums with anniversary this week
   def find_all
-    weekly_albums.sort { |x,y| x.anniversary.current <=> y.anniversary.current }
+    Rails.cache.fetch(weekly_album_cache_key) do
+      weekly_albums.sort do |x,y|
+        x.anniversary.current <=> y.anniversary.current
+      end
+    end
   end
 
   private
 
   def weekly_albums
     @relation.select { |album| @week.include?(album.anniversary.current) }
+  end
+
+  def weekly_album_cache_key
+    count          = @relation.count
+    max_updated_at = @relation.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "albums/all-#{count}-#{max_updated_at}-#{@week.number}"
   end
 
 end
