48f97a3bb1af47936ee9152ed2772a04579ec0c2
Ryan Norman <rsnorman15@gmail.com>
2016-11-30 00:50:21 -0500

    Adds classes to follow accounts that recently interacted

---
+++ lib/services/recent_interactor_follower.rb
@@ -0,0 +1,33 @@
+require_relative 'recent_twitter_interactors'
+
+# Follows accounts that have recently interacted with system account
+class RecentInteractorFollower
+  def self.follow_all
+    new.follow_all
+  end
+
+  def initialize(twitter_client: WistfulIndie::Twitter::Client.client,
+                 screen_names: RecentTwitterInteractors.screen_names)
+    @client = twitter_client
+    @screen_names = screen_names
+  end
+
+  def follow_all
+    unfollowed_screen_names.each do |screen_name|
+      begin
+        puts "Following recent interactor: #{screen_name}"
+        @client.follow(screen_name)
+      rescue Twitter::Error::Forbidden
+        Rails.logger.info 'Cannot follow system account'
+      rescue Twitter::Error::NotFound
+        Rails.logger.error "Account does not exist: #{screen_name}"
+      end
+    end
+  end
+
+  private
+
+  def unfollowed_screen_names
+    @screen_names - TwitterFollow.pluck(:screen_name)
+  end
+end
